bfacfe53eab1955e20b208a8e210017a4737b84e
Vladislau <vladislaugood@gmail.com>
2022-02-23 23:29:30 +0300

    Добавьте файлы проекта.

---
+++ TagGame/Controllers/TagGameController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TagGame.Controllers
+{
+    public class TagGameController : Controller
+    {
+        public IActionResult Start()
+        {
+            return View();
+        }
+
+        public IActionResult Play()
+        {
+            return View();
+        }
+
+    }
+}
